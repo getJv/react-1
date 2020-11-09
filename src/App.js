f2d1806ba60d2709903d9a80e4269dbc35db89e3
Jhonatan Vinicius Paulino de Morais <jhonatan.morais@epl.gov.br>
2020-11-09 16:57:27 -0300

    50 percent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-function App() {
-  return (
-    <form>
-      <input type="text" />
-      <textarea name="" id="" cols="30" rows="10"></textarea>
-    </form>
-  );
+import ListaDeNotas from "./components/ListaDeNotas/ListaDeNotas.jsx";
+import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro.jsx";
+import React, { Component } from "react";
+
+import "./assets/index.css";
+import "./assets/App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <section className="conteudo">
+        <FormularioCadastro />
+        <ListaDeNotas />
+      </section>
+    );
+  }
 }
 
 export default App;
